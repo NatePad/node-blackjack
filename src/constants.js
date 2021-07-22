b0cac283af8c8a0e25cd33135bd45f6effb32ace
Nathan Sartain <nathanmsartain@gmail.com>
2021-07-22 12:20:53 -0400

    add blackjack 21 num to constants

--- src/constants.js
+++ src/constants.js
@@ -4,6 +4,7 @@ module.exports = {
   SUITS: ["♠", "♥", "♦", "♣"],
   FACES: ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"],
   PLAYERS: ["Player", "Dealer"],
+  BLACKJACK: 21,
 
   LOGO: `
 .------.            _     _            _    _            _    
