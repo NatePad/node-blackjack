2a365c3f3c22a32bf647dd99556a80ade648ada0
Nathan Sartain <nathanmsartain@gmail.com>
2021-07-22 12:21:15 -0400

    add getWinner function

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const Deck = require("./src/classes/deck");
 const Hand = require("./src/classes/hand");
 const { getPlayAgain, getHitOrStand } = require("./src/inquirerQuestions");
-const { HIT_OR_STAND, LOGO, PLAY_AGAIN, PLAYERS } = require("./src/constants");
+const { BLACKJACK, LOGO, PLAYERS } = require("./src/constants");
 
 const hands = [];
 
@@ -11,6 +11,51 @@ let dealerWins = 0;
 let playerWins = 0;
 let ties = 0;
 let playAgain = true;
+
+const getWinner = () => {
+  const playerScore = hands[0].getValue();
+  const dealerScore = hands[1].getValue();
+
+  if (playerScore > BLACKJACK) {
+    if (dealerScore > BLACKJACK) {
+      ties++;
+      console.log("You both bust! It's a draw!");
+    } else {
+      dealerWins++;
+      console.log("You bust. The dealer wins");
+    }
+  } else if (dealerScore > BLACKJACK) {
+    playerWins++;
+    console.log("The dealer bust! You win!");
+  } else if (playerScore > dealerScore) {
+    playerWins++;
+    console.log("You win!");
+  } else if (playerScore < dealerScore) {
+    dealerWins++;
+    console.log("The dealer wins.");
+  } else {
+    if (
+      playerScore !== BLACKJACK ||
+      (hands[0].cards.length > 2 && hands[1].cards.length > 2)
+    ) {
+      ties++;
+      console.log("It's a tie!");
+    } else {
+      if (hands[0].cards.length === 2) {
+        if (hands[1].cards.length === 2) {
+          ties++;
+          console.log("You both got Blackjack! It's a draw!");
+        } else {
+          playerWins++;
+          console.log("You break the tie with a Blackjack! Congratulations!");
+        }
+      } else {
+        dealerWins++;
+        console.log("The dealer breaks the tie with a Blackjack!");
+      }
+    }
+  }
+};
 
 const printHands = () => {
   // PRINT THE DEALER'S CARDS FIRST
@@ -44,7 +89,7 @@ const init = async () => {
           // PLAYER HIT/STAY OPERATIONS
           if (await getHitOrStand()) {
             hand.addCards(deck.deal(1));
-            hand.printCards();
+            printHands();
           } else {
             hand.setHit(false);
           }
@@ -55,6 +100,8 @@ const init = async () => {
         }
       }
     }
+
+    getWinner();
 
     playAgain = await getPlayAgain();
   }
